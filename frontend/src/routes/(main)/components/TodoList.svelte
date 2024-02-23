421e30c59b31e2f65c2b170e1f57d135aef97ac8
Roy Tang <hkmute@gmail.com>
2024-02-24 00:30:52 +0800

    Add drag indicator

--- frontend/src/routes/(main)/components/TodoList.svelte
+++ frontend/src/routes/(main)/components/TodoList.svelte
@@ -13,6 +13,7 @@
 	export let items: Todo[];
 	export let status: TodoStatus;
 
+	let dragging = false;
 	let scrolledY = 0;
 	let dropPosition = items.length;
 
@@ -30,6 +31,7 @@
 	};
 
 	const handleDrop = (e: DragEvent) => {
+		dragging = false
 		if (!e.dataTransfer) return;
 		const todo = JSON.parse(e.dataTransfer.getData('application/json')) as Todo;
 		const prevTodo = $todoStore.todoLists[todo.status][dropPosition - 1];
@@ -42,17 +44,31 @@
 	};
 
 	const handleDragover = (e: DragEvent) => {
+		dragging = true;
 		const currentTarget = e.currentTarget as HTMLDivElement;
 		scrolledY = currentTarget.scrollTop;
 	};
 
 	const handleDragoverItem = (index: number) => (e: DragEvent) => {
+		dragging = true;
 		const target = e.target as HTMLDivElement;
 		const middlePoint = target.offsetTop + target.offsetHeight / 2;
 		if (e.clientY + scrolledY > middlePoint) {
 			dropPosition = index + 1;
 		} else {
 			dropPosition = index;
+		}
+	};
+
+	const handleDragoverContainer = (e: DragEvent) => {
+		if (e.target && (e.target as HTMLDivElement).getAttribute('role') === 'list') {
+			dragging = true;
+		}
+	};
+
+	const handleDragLeave = (e: DragEvent) => {
+		if (e.target && (e.target as HTMLDivElement).getAttribute('role') === 'list') {
+			dragging = false;
 		}
 	};
 
@@ -64,9 +80,17 @@
 	};
 </script>
 
-<div class="flex flex-1 flex-col overflow-hidden rounded-2xl bg-indigo-100 pb-0">
+<div
+	class="flex flex-1 flex-col overflow-hidden rounded-2xl bg-indigo-100 pb-0"
+	role="list"
+	on:dragleave={handleDragLeave}
+	on:dragover|preventDefault={handleDragoverContainer}
+>
 	<div
 		class={`mx-4 flex items-center justify-between border-b-4 pb-2.5 pt-4 text-lg font-semibold ${colors[color]}`}
+		role="list"
+		on:drop={handleDrop}
+		on:dragover|preventDefault={() => (dropPosition = 0)}
 	>
 		<div class="px-2">{title}</div>
 		<button
@@ -84,7 +108,7 @@
 	>
 		{#each items as todo, index (todo.id)}
 			<div
-				class="py-2"
+				class={`border-t-2 py-2 ${dragging && dropPosition === index ? 'border-red-900' : 'border-transparent'}`}
 				role="listitem"
 				on:dragover|preventDefault={handleDragoverItem(index)}
 				animate:flip={{ duration: 300 }}
@@ -94,5 +118,6 @@
 				<TodoCard {todo} on:toggle />
 			</div>
 		{/each}
+		<div class={`border-t-2 ${dragging && dropPosition === items.length ? 'border-red-900' : 'border-transparent'}`} />
 	</div>
 </div>
